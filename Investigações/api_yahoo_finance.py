335316ba15938fc99bbfc50fc919d1592da660a5
ivan <ivanbarcelos90@gmail.com>
2022-03-17 08:08:26 -0300

    API yfinance: Init parmeter added

--- Investigações/api_yahoo_finance.py
+++ Investigações/api_yahoo_finance.py
@@ -4,77 +4,74 @@ import yfinance as yf
 class APIStock:
     def __init__(self, stock):
         self.stock = stock
-
-    # The Ticker Module
-    def Ticker(self):
-        return yf.Ticker(self.stock)
+        self.ticker = yf.Ticker(self.stock)
 
     # Get stock info
     def info(self):      
-        return print(self.Ticker().info) 
+        return print(self.ticker.info) 
 
     # Get historical market data
     def history(self):
-        return print(self.Ticker().history(period="max"))
+        return print(self.ticker.history(period="max"))
 
     # Show actions (dividends, splits)
     def actions(self):
-        return print(self.Ticker().actions)
+        return print(self.ticker.actions)
 
     # show dividends
     def dividends(self):
-        return print(self.Ticker().dividends)
+        return print(self.ticker.dividends)
 
     # show splits
     def splits(self):
-        return print(self.Ticker().splits)
+        return print(self.ticker.splits)
 
     # show financials
     def financials(self):
-        return print(self.Ticker().financials), print(self.Ticker().quarterly_financials)
+        return print(self.ticker.financials), print(self.ticker.quarterly_financials)
 
     # show major holders
     def major_holder(self):
-        return print(self.Ticker().major_holders)
+        return print(self.ticker.major_holders)
 
     # show institutional holders
     def institutional_holders(self):
-        return print(self.Ticker().institutional_holders)
+        return print(self.ticker.institutional_holders)
 
     # show balance sheet
     def balance_sheet(self):
-        return print(self.Ticker().balance_sheet), print(self.Ticker().quarterly_balance_sheet)
+        return print(self.ticker.balance_sheet), print(self.ticker.quarterly_balance_sheet)
 
     # show cashflow
     def cashflow(self):
-        return print(self.Ticker().cashflow), print(self.Ticker().quarterly_cashflow)
+        return print(self.ticker.cashflow), print(self.ticker.quarterly_cashflow)
 
     # show earnings
     def earnings(self):
-        return print(self.Ticker().earnings), print(self.Ticker().quarterly_earnings)
+        return print(self.ticker.earnings), print(self.ticker.quarterly_earnings)
 
     # show sustainability
     def sustainability(self):
-        return print(self.Ticker().sustainability)
+        return print(self.ticker.sustainability)
 
     # show analysts recommendations
     def analysts_recommendations(self):
-        return print(self.Ticker().recommendations)
+        return print(self.ticker.recommendations)
 
     # show next event (earnings, etc)
     def calendar(self):
-        return print(self.Ticker().calendar)
+        return print(self.ticker.calendar)
 
     # ISIN = International Securities Identification Number
     def ISIN(self):
-        return print(self.Ticker().isin)
+        return print(self.ticker.isin)
 
     # show options expirations
     def options(self):
-        return print(self.Ticker().options)
+        return print(self.ticker.options)
 
     # show news
     def news(self):
-        return print(self.Ticker().news)
+        return print(self.ticker.news)
 
 
