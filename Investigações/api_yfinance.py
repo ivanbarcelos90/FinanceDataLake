0198f3782dfda934152a7bb896fc5b0b6d90c472
ivan <ivanbarcelos90@gmail.com>
2022-03-31 20:09:48 -0300

    API yfinance: Added generator

--- Investigações/api_yfinance.py
+++ Investigações/api_yfinance.py
@@ -4,27 +4,42 @@ from pathlib import Path
 from os.path import abspath
 from datetime import datetime
 
+
+def create_ticker_df(ticker:str, prd:str = '1d', intv:str = '1d') -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        ticker (str): _description_
+        prd (str, optional): _description_. Defaults to '1d'.
+        intv (str, optional): _description_. Defaults to '1d'.
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+
+    data = yf.download(ticker, group_by="Ticker", period=prd, interval=intv)
+    data['ticker'] = ticker  # add this column because the dataframe doesn't contain a column with the ticker
+    data['Event'] = datetime.now() 
+    data.reset_index(inplace=True)   
+    return data
+
+def import_daily_stock(tickerStrings):
+
+    gen = (create_ticker_df(t) for t in tickerStrings)     
+
+    df = pd.concat(gen)
+
+    df.to_csv(abspath(f'./Investigações/Stock_Data/{datetime.now().strftime("%Y_%m_%d_%H%M%S")}_stock_all.csv'), index=False)  
+
 # Send Every Ticker Symbol to df
 # Csv stock symbol file source: https://www.nasdaq.com/market-activity/stocks/screener
 p = Path(abspath('./Investigações/Stock_List/Stock_Symbol.csv'))
 
 # Create a dataframe to store all stocks symbols
-symbol = pd.read_csv(p, nrows=8188) # nrows got 8188 symbols
+symbol = pd.read_csv(p, nrows=20) # nrows got 8188 symbols
 
 # Parameters to iterate all tickers a store all data in separate csv.
 # tickerStrings = ['AAPL', 'MSFT'] # Test symbol
 tickerStrings = symbol['Symbol'] # All symbols
 
-prd = '1d'
-intv = '1d'
-df = pd.DataFrame()
-for ticker in tickerStrings:
-    data = yf.download(ticker, group_by="Ticker", period=prd, interval=intv)
-    data['ticker'] = ticker  # add this column because the dataframe doesn't contain a column with the ticker
-    data['Event'] = datetime.now()
-    data.reset_index(inplace=True) 
-    # df = df.append(data)
-    df = [df for df in data]
-
-df.to_csv(abspath(f'./Investigações/Stock_Data/{datetime.now().strftime("%Y_%m_%d_%H%M%S")}_stock_all.csv'), index=False)  
-      
+import_daily_stock(tickerStrings)
