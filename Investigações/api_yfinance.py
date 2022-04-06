587596f6905d7ab6b3068f3075faa6bf8b59145c
ivan <ivanbarcelos90@gmail.com>
2022-04-06 20:48:51 -0300

    API yfinance v1.0

--- Investigações/api_yfinance.py
+++ Investigações/api_yfinance.py
@@ -4,42 +4,31 @@ from pathlib import Path
 from os.path import abspath
 from datetime import datetime
 
+def import_daily_stock(ticker:list(), prd:str='1d', intv:str='1d'):
 
-def create_ticker_df(ticker:str, prd:str = '1d', intv:str = '1d') -> pd.DataFrame:
-    """_summary_
+    df_list = list()
+    df = yf.download(tickers=tickerStrings, group_by="Ticker", period=prd, interval=intv, threads=True)
 
-    Args:
-        ticker (str): _description_
-        prd (str, optional): _description_. Defaults to '1d'.
-        intv (str, optional): _description_. Defaults to '1d'.
-
-    Returns:
-        pd.DataFrame: _description_
-    """
-
-    data = yf.download(ticker, group_by="Ticker", period=prd, interval=intv)
-    data['ticker'] = ticker  # add this column because the dataframe doesn't contain a column with the ticker
-    data['Event'] = datetime.now() 
-    data.reset_index(inplace=True)   
-    return data
-
-def import_daily_stock(tickerStrings):
-
-    gen = (create_ticker_df(t) for t in tickerStrings)     
-
-    df = pd.concat(gen)
-
-    df.to_csv(abspath(f'./Investigações/Stock_Data/{datetime.now().strftime("%Y_%m_%d_%H%M%S")}_stock_all.csv'), index=False)  
-
+    for ticker in tickerStrings:
+        df_ticker = df[ticker]
+        df_ticker['ticker'] = ticker  
+        df_ticker['Event'] = datetime.now()    
+        df_list.append(df_ticker)
+        
+    df_total = pd.concat(df_list).reset_index()
+    df_total.to_csv(abspath(f'./Investigações/Stock_Data/{datetime.now().strftime("%Y_%m_%d_%H%M%S")}_stock_all.csv'), index=False)  
+           
 # Send Every Ticker Symbol to df
 # Csv stock symbol file source: https://www.nasdaq.com/market-activity/stocks/screener
 p = Path(abspath('./Investigações/Stock_List/Stock_Symbol.csv'))
 
 # Create a dataframe to store all stocks symbols
-symbol = pd.read_csv(p, nrows=20) # nrows got 8188 symbols
+symbol = pd.read_csv(p, nrows=8188) # nrows got 8188 symbols
 
 # Parameters to iterate all tickers a store all data in separate csv.
-# tickerStrings = ['AAPL', 'MSFT'] # Test symbol
+# tickerStrings = ['AAPL','MSFT','TWTR','F','BBD','ITUB','VALE','NVDA','T','PBR','FB','TSLA','PFE','GM','INTC','UBER','KO','HPQ','DIS','CIG'] # Test symbol
 tickerStrings = symbol['Symbol'] # All symbols
+tickerStrings = tickerStrings.to_list()
 
-import_daily_stock(tickerStrings)
+import_daily_stock(tickerStrings, '1d','1d')
+    
